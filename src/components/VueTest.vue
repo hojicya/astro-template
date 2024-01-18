85f875eb523584abb472e268d3ba56cf541664c0
hojicya <hojicya01@gmail.com>
2024-01-18 11:56:41 +0900

    Add code formatter

--- src/components/VueTest.vue
+++ src/components/VueTest.vue
@@ -4,7 +4,7 @@
       <input type="test" v-model="text" class="form-control-inline col-6" />
       <div class="col-5">
         <button @click="show()" class="btn btn-primary">show</button>
-        <button @click="chanege()" class="btn btn-success">change</button>
+        <button @click="change()" class="btn btn-success">change</button>
       </div>
       <div class="alert alert-info mt-3">{{ text }}</div>
     </div>
