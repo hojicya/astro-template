553c7d715a0b621dfb338afb1b8fff5dfb3f0142
hojicya <hojicya01@gmail.com>
2024-01-18 11:32:10 +0900

    add scss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,41 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  compressHTML: true,
+  build: {
+    assets: "assets",
+    inlineStylesheets: "never",
+  },
+  vite: {
+    build: {
+      minify: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: "assets/js/[name].js",
+          assetFileNames: ({ name }) => {
+            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+              return "assets/images/[name].[ext]";
+            }
+            if (/\.css$/.test(name ?? "")) {
+              return "assets/css/[name].[ext]";
+            }
+            return "assets/[name].[ext]";
+          },
+        },
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "src/styles/base/_variables.scss" as *;
+            @use "src/styles/base/_mixin.scss" as *;
+            `,
+        },
+      },
+    },
+  },
 });
