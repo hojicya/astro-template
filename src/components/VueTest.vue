6e1baf5c9a876f88c62b8ca89b309ba87edb1d71
hojicya <hojicya01@gmail.com>
2024-01-18 10:20:42 +0900

    Add vue

---
+++ src/components/VueTest.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row mt-3">
+      <input type="test" v-model="text" class="form-control-inline col-6" />
+      <div class="col-5">
+        <button @click="show()" class="btn btn-primary">show</button>
+        <button @click="chanege()" class="btn btn-success">change</button>
+      </div>
+      <div class="alert alert-info mt-3">{{ text }}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    show: function () {
+      alert(this.text);
+    },
+    chanege: function () {
+      this.text = "test!!!";
+    },
+  },
+};
+</script>
+
+<style></style>
